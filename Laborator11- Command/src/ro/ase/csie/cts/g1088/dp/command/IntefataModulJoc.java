793e092ec0bf4fe6b5deb9b4b44a102ac1d31139
marius.tudorache3@gmail.com <Catalyst32>
2021-05-13 13:23:37 +0300

    da

---
+++ Laborator11- Command/src/ro/ase/csie/cts/g1088/dp/command/IntefataModulJoc.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.g1088.dp.command;
+
+public interface IntefataModulJoc {
+
+    public void executaTask(String detalii);
+}
---
+++ Laborator11- Command/src/ro/ase/csie/cts/g1088/dp/command/ModulUpdate.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1088.dp.command;
+
+public class ModulUpdate implements IntefataModulJoc{
+    @Override
+    public void executaTask(String detalii) {
+        System.out.println("Start update joc pentru versiunea " + detalii);
+    }
+}
