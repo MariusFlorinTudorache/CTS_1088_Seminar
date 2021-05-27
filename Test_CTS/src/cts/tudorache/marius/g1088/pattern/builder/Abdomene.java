68a317c9c7b0de141663f3c387bff81ce6887f21
marius.tudorache3@gmail.com <Catalyst32>
2021-05-27 12:44:55 +0300

    part1

---
+++ Test_CTS/src/cts/tudorache/marius/g1088/pattern/builder/Abdomene.java
@@ -0,0 +1,8 @@
+package cts.tudorache.marius.g1088.pattern.builder;
+
+public class Abdomene implements InterfataExercitiu {
+    @Override
+    public void exercitiu() {
+        System.out.println("Curs de abdomene");
+    }
+}
